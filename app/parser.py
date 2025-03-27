147ae1a656123624235f7b74d368eea6b848994c
hajerkr <heajer.karoui@gmail.com>
2025-03-27 13:22:21 +0100

    Imputing values from session info and session object

--- app/parser.py
+++ app/parser.py
@@ -24,7 +24,8 @@ def parse_config(context):
 
     api_key = context.get_input("api-key").get("key")
     fw = flywheel.Client(api_key=api_key)
-    user = fw.get_current_user().email
+    user = f"{fw.get_current_user().firstname} {fw.get_current_user().lastname} [{fw.get_current_user().email}]"
+
     print(f"Logged in as {fw.get_current_user().email}")
 
     input_container = context.client.get_analysis(context.destination["id"])
@@ -45,30 +46,135 @@ def parse_config(context):
 
     age_min = context.config.get("age_min")
     age_max = context.config.get("age_max")
+    age_range = context.config.get("age_range")
     threshold = context.config.get("threshold")
 
     # -------------------  Get Input label -------------------  #
 
     # Specify the directory you want to list files from
-    directory_path = '/flywheel/v0/input/input'
+    input_path = '/flywheel/v0/input/input'
     # List all files in the specified directory
     #recon-all output, QC output , [add more as needed]
     input_labels = {} 
+    name_key_maping = {
+    "recon-all-clinical": "volumetric",
+    "synthseg": "volumetric",
+    "mrr_axireg": "volumetric"
+}
 
-    for filename in os.listdir(directory_path):
-        if "recon-all-clinical" in filename:
-            input_labels["recon-all"] = filename
-
-        elif "parsed_qc_annotations" in filename:
-            input_labels["qc"] = filename
-
-        elif "synthseg" in filename:
-            input_labels["synthseg"] = filename
-        
-        elif "ants-segementation" in filename:
-            input_labels["ants-segementation"] = filename
+    for filename in os.listdir(input_path):
+    #for filename in file_inputs: #This line was used when debugging locally and specifying filenames
+        for keyword, key in name_key_maping.items():
+            if keyword in filename:
+                # if key == "qc":
+                #     input_labels['qc'] = filename
+                # else:
+                input_labels['volumetric'] = filename
+                #break
 
     print("Input files found: ", input_labels)
 
-    return user, df, input_labels, age_min, age_max, threshold, project_label, directory_path
+    impute_information(context,input_labels['volumetric'])
+    rename_columns (input_labels['volumetric'])
+
+    return user, df, input_labels, age_range, age_min, age_max, threshold, project_container, input_path, api_key
+
+
+def impute_information(context,vols):
+
+    """Imputes missing information needed for the plotting functions. 
+    Currently it handles sex (Add as needed)
+
+    Returns:
+        imputed file
+    """
+
+    
+    api_key = context.get_input("api-key").get("key")
+    fw = flywheel.Client(api_key=api_key)
+
+    input_container = context.client.get_analysis(context.destination["id"])
+    proj_id = input_container.parents["project"]
+    project_container = context.client.get(proj_id)
+    project_label = project_container.label
+    
+    project = fw.projects.find_first(f'label="{project_label}"')
+
+    input_path = '/flywheel/v0/input/input'
+    output_path = '/flywheel/v0/output'
+    df = pd.read_csv(os.path.join(input_path,vols))
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+
+    # Check if the sex or age column has empty values
+    columns = ['sex','age']
+    #has_empty_values = df[column_name].isnull().any()
+
+    
+
+    #For every session get the sex information for those with missing
+    for index, row in df.iterrows():
+        subject_label = row['subject']
+        session_label = row['session']
+        # Find the subject by label
+        subject = next((s for s in project.subjects() if s.label == subject_label), None)
+
+        
+        subject = subject.reload()
+        session = next((s for s in subject.sessions() if s.label == session_label), None)
+        session = session.reload()
+
+        for column_name in columns:
+            if column_name == 'sex':
+                df.at[index, column_name] = subject.sex
+                print(f"Imputing {column_name} for subject: {subject_label}: {subject.sex}")
+            else:
+                #get either session information (age_months) session
+                if session.info != {}:
+                    key_with_age = [key for key in session.info if 'age' in key]
+                    age = session.info[key_with_age[0]]
+                    df.at[index, column_name] = age
+                    
+                    print(f"Imputing {column_name} for subject: {subject_label}: {age}")
+                else:  
+                    df.at[index, column_name] = session.age_years * 12 #Flywheel session age is in years, convert to months
+                    print(f"Imputing {column_name} for subject: {subject_label}: {session.age_years * 12}")
+
+    #Printing the modified csv to the input directory to be used for plotting
+    df.to_csv(os.path.join(input_path,vols),index=False)
+
+    #saving those with missing sex/age information to a separate file
+    missing_info = df[df.isnull().any(axis=1)][['subject','session','age','sex','acquisition']]
+
+    #if data frame is not empty, save it
+    if missing_info.empty == False:
+        missing_info.to_csv(os.path.join(output_path,"missing_sex_age_info.csv"),index=False)
+
+def rename_columns (vols):
+
+    print('RENAMING COLUMNS....')
+
+    input_path = '/flywheel/v0/input/input'
+    df = pd.read_csv(os.path.join(input_path,vols))
+    
+
+    name_key_maping = {
+    "recon-all":{'total intracranial': 'total intracranial'},
+    "synthseg": {'total intracranial': 'total intracranial'},
+    "mrr_axireg":{'icv': 'total intracranial'}}
+
+
+    for keyword, key in name_key_maping.items():
+        if keyword in vols:
+            column_mapping = name_key_maping[keyword]
+            df.rename(columns=column_mapping,inplace=True)
+            print('Column has been renamed')
+        
+
+    df.columns = df.columns.str.replace('_', ' ').str.replace('-', ' ').str.lower()
+    df.to_csv(os.path.join(input_path,vols),index=False)
+    print(os.path.join(input_path,vols))
+    df.to_csv(os.path.join(input_path,"updated_headers.csv"),index=False)
+
+    print("file saved...")
 
--- run.py
+++ run.py
@@ -25,20 +25,21 @@ log = logging.getLogger(__name__)
 # Define the main function
 def main(context: GearToolkitContext) -> None:
 
-    output_dir= "/flywheel/v0/work/"
+    output_dir= "/flywheel/v0/output/"
+    work_dir= "/flywheel/v0/work/"
 
     # Step 0: Parse the configuration file
-    user, filepath, input_labels, age_min, age_max, threshold, project_label, directory_path = parse_config(context)
+    user, filepath, input_labels, age_range, age_min, age_max, threshold, project, directory_path,api_key = parse_config(context)
 
     # Step 1: Create the cover page
-    cover = create_cover_page(user, input_labels, age_min, age_max, threshold, project_label,output_dir)
-
+    cover , age_min, age_max = create_cover_page(user, input_labels, age_range, age_min, age_max, threshold, project,work_dir)
+    project_label = project.label
     # Step 2: Parse the CSV file
-    df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels = parse_csv(filepath, project_label, age_min, age_max, threshold)
+    df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels = parse_csv(filepath, project_label, age_range, age_min, age_max, threshold)
 
     # Step 3: Create the data report using the parsed CSV, and the QC csv    
-    report = create_data_report(df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels, age_min, age_max, threshold,output_dir)
-    qc = generate_qc_report(directory_path, input_labels, output_dir,project_labels)
+    report = create_data_report(df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels, age_range, age_min, age_max, threshold,output_dir, api_key)
+    # qc = generate_qc_report(directory_path, input_labels, output_dir,project_labels)
     # Step 4: Merge cover page and data report
     # Get the current timestamp
     current_timestamp = datetime.now()
@@ -47,7 +48,7 @@ def main(context: GearToolkitContext) -> None:
 
     final_report = os.path.join(output_dir, f"{project_label}_{formatted_timestamp}_report.pdf")
 
-    merge_pdfs(cover, report, qc, final_report)
+    merge_pdfs(cover, report, final_report)
 
     print(f"Report generated: {final_report}")
 
